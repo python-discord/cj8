63c8318fb6294db4963e3901e19ed4075603646c
Fergus <fergusbhaak@gmail.com>
2021-07-10 16:51:06 +0100

    Update framework_interface.py

--- project/framework_interface.py
+++ project/framework_interface.py
@@ -19,37 +19,71 @@ def list_files(startpath):
 
 def start():
     list_files("OS")
+    print(term.black_on_darkkhaki("─────────CMD─────────"))
 
 def printLogo():
     print()
-    print(term.black_on_darkkhaki("┌────────────────CMD"))
+    print(term.black_on_darkkhaki(""))
     schedule()
 
 def schedule():
     schedule = True
     while schedule == True:
-        userInput = input(">>>  ")
+        userInput = input(">>>  ").lower()
         if userInput[:4] == "help":
             startHelp(userInput)
             schedule = False
         if userInput[:3] == "add":
             startAdd(userInput)
             schedule = False
+        if userInput[:3] == "dir":
+            startDir(userInput)
+            schedule = False
 
 def startHelp(input):
-    print(term.black_on_darkkhaki("┌────────────────Help"))
-    print(term.black_on_darkkhaki("│help (command)"))
-    print(term.black_on_darkkhaki("│add [name] [dd/mm/yyyy] [time (13:12)]  (desc)"))
-    print(term.black_on_darkkhaki("│remove [name]"))
     input = input.split()
-
+    if input[1] == "add":
+        print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
+        print(term.black_on_darkkhaki("│add [name] [dd/mm/yyyy] [time (13:12)]  (desc)│"))
+        print(term.black_on_darkkhaki("│- creates a new file with a specified name in │"))
+        print(term.black_on_darkkhaki("│  specified directory                         │"))
+        print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
+    if input[1] == "remove":
+        print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
+        print(term.black_on_darkkhaki("│remove [name]                                 │"))
+        print(term.black_on_darkkhaki("│- deletes a  file with a specified name in    │"))
+        print(term.black_on_darkkhaki("│  specified directory                         │"))
+        print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
+    if input[1] == "dir":
+        print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
+        print(term.black_on_darkkhaki("│dir                                           │"))
+        print(term.black_on_darkkhaki("│- shows full  user directory                  │"))
+        print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
+    if input[1] == "help":
+        print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
+        print(term.black_on_darkkhaki("│help (command)                                │"))
+        print(term.black_on_darkkhaki("│- explains what the specified command does    │"))
+        print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
+    else:
+        print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
+        print(term.black_on_darkkhaki("│help (command)                                │"))
+        print(term.black_on_darkkhaki("│add [name] [dd/mm/yyyy] [time (13:12)]  (desc)│"))
+        print(term.black_on_darkkhaki("│remove [name]                                 │"))
+        print(term.black_on_darkkhaki("│dir                                           │"))
+        print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
     printLogo()
 
 def startAdd(input):
-    print(term.black_on_darkkhaki("┌────────────────Add"))
+    print(term.black_on_darkkhaki("┌─────────ADD─────────┐"))
+    print(term.black_on_darkkhaki("└─────────────────────┘"))
     input = input.split()
     
     printLogo()
+
+def startDir(input):
+    print(term.black_on_darkkhaki("─────────DIR─────────"))
+    list_files("OS")
+    input = input.split()
 
 if __name__ == "__main__":
     start()
