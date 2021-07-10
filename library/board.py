bb99eeea575ed214dc34edbd26b8f414f677827b
A5rocks <git@helvetica.moe>
2021-07-11 04:42:26 +0900

    Finish basic tic-tac-toe game.

--- library/board.py
+++ library/board.py
@@ -24,7 +24,8 @@ class Board:
     """Represents the board that the game of tic-tac-toe is played on."""
 
     contents: tuple[Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell]
-    turn: typing.Literal["X", "O"]
+    player1_turn: bool
+    game_over: bool
 
     def __init__(self) -> None:
         self.contents = (
@@ -38,13 +39,21 @@ class Board:
             Cell(),
             Cell(),
         )
-        self.turn = "O"
+        self.player1_turn = True
+        self.game_over = False
 
     def handle_input(self, number: typing.Literal[0, 1, 2, 3, 4, 5, 6, 7, 8]) -> None:
         """Handles updating the board when it needs to be updated."""
-        self.contents[number].contents = self.turn
+        if self.game_over:
+            return
 
-        self.turn = "O" if self.turn == "X" else "X"
+        if self.contents[number].contents != " ":
+            return
+
+        self.contents[number].contents = "O" if self.player1_turn else "X"
+        self.player1_turn = not self.player1_turn
+
+        self.game_over = self._won_wrapper()
 
     def display(self, term: blessed.Terminal) -> str:
         """Gives a colored display of the board."""
@@ -55,3 +64,26 @@ class Board:
             result += "\n"
 
         return result
+
+    def _won_wrapper(self) -> bool:
+        return self._won(tuple(content.contents for content in self.contents))
+
+    def _won(self, board: tuple[typing.Literal["X", "O", " "], ...]) -> bool:
+        if any(
+            board[3 * i : 3 * (i + 1)] in (("X", "X", "X"), ("O", "O", "O"))
+            for i in range(3)
+        ):
+            return True
+
+        if any(
+            board[3 * i :: 3] in (("X", "X", "X"), ("O", "O", "O")) for i in range(3)
+        ):
+            return True
+
+        if board[0] == board[4] == board[8] != " ":
+            return True
+
+        if board[2] == board[4] == board[6] != " ":
+            return True
+
+        return False
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ board = Board()
 # python 3.10 feature, but it works in python 3.9
 with (term.fullscreen(), term.cbreak()):
     print(term.on_grey(board.display(term)), end="")
+    print(f"It is Player {'1' if board.player1_turn else '2'}'s turn!")
 
     while (val := term.inkey()) != "q":
         if val in ("1", "2", "3", "4", "5", "6", "7", "8", "9"):
@@ -19,6 +20,11 @@ with (term.fullscreen(), term.cbreak()):
                 "1", "2", "3",
             ].index(val)
             # fmt: on
-            board.handle_input(value)
+            board.handle_input(value)  # type: ignore
 
         print(term.clear() + term.on_grey(board.display(term)), end="")
+        if not board.game_over:
+            print(f"It is Player {'1' if board.player1_turn else '2'}'s turn!")
+        else:
+            # the last person to take a move before the end has to have won.
+            print(f"Player {'2' if board.player1_turn else '1'} won!")
