1a3284f4f3bf16f610a968cd0fa7d7d59c561f7c
Fergus <fergusbhaak@gmail.com>
2021-07-10 17:26:01 +0100

    Update framework_interface.py

--- project/framework_interface.py
+++ project/framework_interface.py
@@ -16,18 +16,17 @@ def list_files(startpath):
         print(term.black_on_darkkhaki('├'+'{}{}/{}'.format(indent, path.basename(root),extra_blank_needed)))
         subindent ='├' +'─' * 4 * (level + 1)
         for f in files:
-            # current_line_length = len('{}{}'.format(subindent, f))
-            # extra_blank_needed = (BLANK_LINES - current_line_length) * " "
-            print(term.black_on_darkkhaki('{}{}'.format(subindent, f)))
+            current_line_length = len('{}{}'.format(subindent, f))
+            extra_blank_needed = (BLANK_LINES - current_line_length) * " "
+            print(term.black_on_darkkhaki('{}{}{}'.format(subindent, f,extra_blank_needed)))
         
     printLogo()
 
 def start():
-    list_files("project/OS")
+    list_files("OS")
     print(term.black_on_darkkhaki("─────────CMD─────────"))
 
 def printLogo():
-    print()
     print(term.black_on_darkkhaki(""))
     schedule()
 
@@ -35,7 +34,7 @@ def schedule():
     schedule = True
     while schedule == True:
         userInput = input(">>>  ").lower()
-        if userInput[:4] == "help":
+        if userInput[:4] == "help" or userInput[:1] == "h":
             startHelp(userInput)
             schedule = False
         if userInput[:3] == "add":
@@ -46,6 +45,7 @@ def schedule():
             schedule = False
 
 def startHelp(input):
+    input += " 1 2 3 " #place holder inputs which stops the user from entering errors
     input = input.split()
     if input[1] == "add":
         print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
@@ -53,18 +53,18 @@ def startHelp(input):
         print(term.black_on_darkkhaki("│- creates a new file with a specified name in │"))
         print(term.black_on_darkkhaki("│  specified directory                         │"))
         print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
-    if input[1] == "remove":
+    elif input[1] == "remove":
         print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
         print(term.black_on_darkkhaki("│remove [name]                                 │"))
         print(term.black_on_darkkhaki("│- deletes a  file with a specified name in    │"))
         print(term.black_on_darkkhaki("│  specified directory                         │"))
         print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
-    if input[1] == "dir":
+    elif input[1] == "dir":
         print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
         print(term.black_on_darkkhaki("│dir                                           │"))
         print(term.black_on_darkkhaki("│- shows full  user directory                  │"))
         print(term.black_on_darkkhaki("└──────────────────────────────────────────────┘"))
-    if input[1] == "help":
+    elif input[1] == "help":
         print(term.black_on_darkkhaki("┌─────────HELP─────────────────────────────────┐"))
         print(term.black_on_darkkhaki("│help (command)                                │"))
         print(term.black_on_darkkhaki("│- explains what the specified command does    │"))
