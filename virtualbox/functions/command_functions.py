d5dae244f565c8370f2e876947f61691cb5b1ab8
Fergus <fergusbhaak@gmail.com>
2021-07-11 22:53:09 +0100

    Update command_functions.py

--- virtualbox/functions/command_functions.py
+++ virtualbox/functions/command_functions.py
@@ -11,11 +11,8 @@ BLANK_LINES = 50
 
 # COMMAND LIST
 def start_walk(user_input, fs, user):
-    
-    print(fs.stringList(user))
-    print_box("Walk", fs.walk(user))
-
-
+    print(term.green_on_black(print_box("Walk", fs.stringList(user))))
+    print(fs.walk(user))
 
 
 
