66e5345ddf4a1982746976374784cb7f8c71bba2
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2021-07-11 16:57:44 +0200

    Rewritten File

--- main/first_game_dialog.py
+++ main/first_game_dialog.py
@@ -1,24 +1,33 @@
 
 def prinstart(arg):
-    print(term.clear)
+    print(term.home + term.clear + term.move_y(term.height // 2))
     print(term.green_on_black(arg))
     time.sleep(3)
     print(term.green_on_black("Press C to continue"))
     with term.cbreak():
         val = ''
         if val.lower() == 'c' or val.lower() == 'C':
+            time.sleep(1)
             return
         else:
-            continue
+            pass
+
+def gameloads(arg):
+    print(term.green_on_black(arg))
+    time.sleep(2)
+    return
 
 if __name__ == "__main__":
     printstart("""Hey There! \n  You are an Artificial Intelligant, built by the USA, developed to get into PCs and analyze them. \n You was hacked into a System by the Atomic Program of the Iran. Here, ur job was to analyze the Data and to see if there are any files which could gives hint to the Atomatic Missiles of the Iran. \n \n""")
-    printstart("""You found out that there will be a Atomatic launch today, it should hit the US. But unfortunally, the system is offline, you cant contact the USA to warn them. \n \n""")
-    printstart("""Because of that, u decide that ull try to turn of the System, because you found indicates that that will stop the attack. But unfortunally, you need Root Privilages to shutdown the Operating System \n \n""")
-    printstart("""You can gain access to these by (*insert challange here, example: get thr password of the main file*). You will have to overcome multiple challenges \n \n""")
-    vrintstart("""So, dont waste your time, think smarter not harder, and good luck!
-(*Title* starting, 
-gaining system access,
-Access gained.
-AI will launch...) \n \n""")
+    printstart("""You found out that there will be a Atomatic launch today, it should hit the US. But unfortunally, the Computer is offline, you cant contact the USA to warn them. \n \n""")
+    printstart("""Because of that, you decide that you'll try to turn of the System, because you found indicates that that will stop the attack. But unfortunally, you need Root Privilages to shutdown the Operating System \n \n""")
+    printstart("""You can gain access to these by (*insert challange here, example: get the password of the main file*). You will have to overcome multiple challenges \n \n""")
+    printstart("""So, dont waste your time, think smarter not harder, and good luck!""")
+    gameloads("*Title* Starting,")
+    gameloads("Gaining System Access,")
+    for _ in range(10):
+       print(term.green_on_black("-"*_ + "_"*int(_ - 10) + "  " + str(_*10) + "%"))
+       time.sleep(0.2)
+    gameloads("Access Gained,")
+    gameloads("AI launched successfully,")
     
