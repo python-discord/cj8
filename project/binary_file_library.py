1981cd308fe8aabcd256e2ee5dc94f18c9d3c22f
Fergus <fergusbhaak@gmail.com>
2021-07-11 11:07:05 +0100

    removed duplicate

--- project/binary_file_library.py
+++ project/binary_file_library.py
@@ -1,42 +1,66 @@
 #FILE ENCRYPTION LIBRARY - DEVELOPED BY CHEERFUL CHEETAHS (Contributed by Coder400, [PUT YOUR NAME HERE])
 import string
 
+HASH = "abcdef"
+
 #Define our XOR operator.
 def XOR(a, b):
     return (a and not b) or (not a and b)
 
+#Function to edit current hash. (HIGHLY RECOMMENDED)
+def editHash(newHash):
+    global HASH
+    HASH = newHash
 
 #Calculate the value of our hash.
-def getHashValue(password):
+def getHashValue():
     #Initialise our value to 0.
     value = 0
-    for character in password:
+    for character in HASH:
         #Add up the value of the character.
         value += ord(character)
     return value
 
 #Get bytearray from file.
 def generate_Bytearray(filename):
-    try:
-        file = open(filename, 'rb')
-    except FileNotFoundError:
-        return None
+    file = open(filename, 'rb')
     #Convert our bytes into a byte array.
     data = bytearray(file.read())
     file.close()
     return data
 
-# Function to encrypt/decrypt files for the OS. (We only need 1 function because of XOR cipher)
-def modifyFile(filename, password):
-    #Get our bytes to work with.
-    data_bytes = generate_Bytearray(filename)
-    if data_bytes == None:
-        print("File not found!\n")
-        return None
-    for pos,byte in enumerate(data_bytes):
+def modify(byte_array):
+    for pos,byte in enumerate(byte_array):
         #Go into each pos and run XOR on current byte and our HASH
-        data_bytes[pos] = XOR(byte, getHashValue(password))
+        byte_array[pos] = XOR(byte, HASH)
+    return byte_array
+
+# Function to encrypt/decrypt files for the OS. (We only need 1 function because of XOR cipher)
+def modifyFile(filename):
+    #Get our bytes to work with and then modify them.
+    data_bytes = modify(generate_Bytearray(filename))
     file = open(filename, 'wb')
     file.truncate()
     file.write(data_bytes)
     file.close()
+
+def openFile(filename):
+    #Get decrypted bytes.
+    data_bytes = modify(generate_Bytearray(filename))
+    #Decode our message from bytes to a readable format.
+    msg = data_bytes.decode('utf-8')
+    return msg
+
+def writeFile(filename, msg):
+    #Create file on our system.
+    file = open(filename, 'x')
+    file.close()
+    file = open(filename, 'wb')
+    #Convert our message into bytes
+    msg = bytes(msg, 'utf-8')
+    msg = bytearray(msg)
+    #Write bytes into file and then close.
+    file.write(msg)
+    file.close()
+    #Encrypt our file.
+    modifyFile(filename)
