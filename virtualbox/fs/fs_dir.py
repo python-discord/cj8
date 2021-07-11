cda7daee410f38a61dd4c53fef5021c8cc0ecb93
WellIDK <65762932+WellIDKRealy@users.noreply.github.com>
2021-07-11 23:13:22 +0200

    Update fs_dir.py
    fix walk

--- virtualbox/fs/fs_dir.py
+++ virtualbox/fs/fs_dir.py
@@ -100,10 +100,10 @@ class Dir(AC):
 
     def walk(self, user):
         try:
-
-            Result = {}
+            Result = []
             for key, item in self.ls(user).items():
-                Result[key] = item if isinstance(item, File) else item.walk(user)
+                if key != "..":
+                    Result.append(item if isinstance(item, File) else item.walk(user))
 
             return Result
         except PermisionDenied:
