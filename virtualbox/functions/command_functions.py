71a90dee93b2245018eb4dc93ae0cb70444a65a7
Fergus <fergusbhaak@gmail.com>
2021-07-11 18:19:28 +0100

    2 files working together import not working

--- virtualbox/functions/command_functions.py
+++ virtualbox/functions/command_functions.py
@@ -1,37 +1,24 @@
-from .blessed_functions import print_box, print_tree
+from virtualbox.project import clear_term, print_tree, print_box, user_input_cmd
+from os import walk, sep, path
 
 
-# COMMAND LIST
-def start_add(user_input):
-    # print(term.home + term.clear + term.move_y(term.height // 2))
-    # user_input = input.split()
-    # path_l = input[1].split("/")
-    # if not os.path.isdir(path_l[0]):
-    #     os.makedirs(path_l[0])
-    #     open(os.path.join(path_l[0], path_l[1])).close()
-    # else:
-    #     if "." in path_l[0]:
-    #         open(path_l[0]).close()
-    #     else:
-    #         open(os.path.join(path_l[0], path_l[1])).close()
-    #
-    # start_dir("dir")
-    user_input_cmd()
-
+START_PATH = "OS/game_files/"
+BLANK_LINES = 50  # number of characters each line should be
+cl = ["│", "─", "┌", "┬", "┐", "├", "┼", "┤", "└", "┴", "┘"]
 
 def start_dir(user_input):
     user_input += " 1 1 1 "
     user_input = user_input.split()
-    print(term.home + term.clear + term.move_y(term.height // 2))
+    clear_term()
     if user_input[1] == "1":
-        printtree("System", START_PATH)
+        print_tree("System", START_PATH)
     else:
-        printtree("System", f"OS/game_files/{user_input[1]}")
+        print_tree("System", f"OS/game_files/{user_input[1]}")
     user_input_cmd()
 
 
 def start_help(user_input):
-    print(term.home + term.clear + term.move_y(term.height // 2))
+    clear_term()
     user_input += " 1 2 3 "  # place holder inputs which stops the user from entering errors
     user_input = "help add"
     user_input = user_input.split()
@@ -46,16 +33,16 @@ def start_help(user_input):
     }
     if user_input[1] in user_input_dir:
         for heade, lis in user_input_dir[user_input[1]]:
-            print(term.green_on_black(print_box(heade, lis)))
+            print_box(heade, lis)
     else:
-        print(term.green_on_black(print_box("Help", ["help (command)",
-                                                     "add [name]",
-                                                     "remove [name]",
-                                                     "dir",
-                                                     "read [file path]",
-                                                     "quickcrypt [file path] [password]",
-                                                     "search [name]"
-                                                     ])))
+        print_box("Help", ["help (command)",
+                         "add [name]",
+                         "remove [name]",
+                         "dir",
+                         "read [file path]",
+                         "quickcrypt [file path] [password]",
+                         "search [name]"
+                         ])
     user_input_cmd()
 
 
@@ -65,10 +52,12 @@ def start_quickcrypt(user_input):
     user_input = user_input.split()
     binary_file_library.decrypt_file(user_input[1], user_input[2])
 
+    user_input_cmd()
+
 
 def start_read(user_input):
     user_input = user_input.split()
-
+    user_input_cmd()
 
 def start_search(user_input):
     # only searches for the first word after "search"
@@ -90,66 +79,5 @@ def start_search(user_input):
     user_input_cmd()
 
 
-# COMMAND DICTIONARY
-user_commands = {"add": start_add,
-                 "dir": start_dir,
-                 "h": start_help,
-                 "help": start_help,
-                 "quickcrypt": start_quickcrypt,
-                 "read": start_read,
-                 "search": start_search
-                 }
 
 
-# COMMAND MANAGER
-def user_input_cmd():
-    print(term.green_on_black(""))
-    showing_input_menu = True
-    while showing_input_menu:
-        user_input = input(">>>  ").lower()
-        if user_input in user_commands.keys():
-            user_commands[user_input](user_input)
-            showing_input_menu = False
-
-        # if user_input[:4] == "help" or user_input[:1] == "h":
-        #     start_help(user_input)
-        #     showing_input_menu = False
-        # if user_input[:3] == "add":
-        #     start_add(user_input)
-        #     showing_input_menu = False
-        # if user_input[:3] == "dir":
-        #     start_dir(user_input)
-        #     showing_input_menu = False
-        # if user_input[:10] == "quickcrypt":
-        #     start_quickcrypt(user_input)
-        #     showing_input_menu = False
-        # if user_input[:4] == "read":
-        #     start_read(user_input)
-        #     showing_input_menu = False
-        # if user_input[:6] == "search":
-        #     start_search(user_input)
-        #     showing_input_menu = False
-
-        if user_input == "ls":
-            for i in this_dir.ls(User).keys():
-                print(i)
-        if user_input[:2] == "cd":
-            try:
-                this_dir = this_dir.getDir(User(), user_input[3:])
-            except Exception as e:
-                print(e)
-        if user_input[:5] == "mkdir":
-            try:
-                this_dir.mkdir(User, user_input[6:])
-            except Exception as e:
-                print(e)
-        if user_input[:5] == "touch":
-            try:
-                this_dir.touch(User, user_input[6:])
-            except Exception as e:
-                print(e)
-        if user_input[:2] == "rm":
-            try:
-                this_dir.rm(User, user_input[3:])
-            except Exception as e:
-                print(e)
