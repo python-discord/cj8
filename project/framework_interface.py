9bb6b681ea156f3d772e0ba863ca00184be90f3d
Fergus <fergusbhaak@gmail.com>
2021-07-11 14:28:13 +0100

    Update framework_interface.py

--- project/framework_interface.py
+++ project/framework_interface.py
@@ -1,5 +1,6 @@
 from os import walk, path, sep
 from blessed import Terminal
+import time
 
 # local modules
 import binary_file_library
@@ -42,6 +43,19 @@ def printtree(header, location):
     print(term.green_on_black(f'└─ /{header}/' + ('─' * (BLANK_LINES - 6 - len(header))) + '┘'))
 
 
+def printstart(arg):
+    print(term.clear)
+    print(term.green_on_black(arg))
+    time.sleep(3)
+    print(term.green_on_black("Press C to continue"))
+    with term.cbreak():
+        val = ''
+        if val.lower() == 'c':
+            return
+        else:
+            pass
+
+
 def print_box(header, textl):
     def print_box_str(ret_string, words):
         # if fits in one line
@@ -75,9 +89,23 @@ def print_box(header, textl):
 
 
 def start():
-    printtree("System", START_PATH)
-    user_input_cmd()
 
+    if __name__ == "__main__":
+        printstart(
+            """Hey There! \n  You are an Artificial Intelligant, built by the USA, developed to get into PCs and analyze them. \n You was hacked into a System by the Atomic Program of the Iran. Here, ur job was to analyze the Data and to see if there are any files which could gives hint to the Atomatic Missiles of the Iran. \n \n""")
+        printstart(
+            """You found out that there will be a nuclear launch today, it should hit the US. But unfortunally, the system is offline, you cant contact the USA to warn them. \n \n""")
+        printstart(
+            """Because of that, u decide that ull try to turn of the System, because you found indicates that that will stop the attack. But unfortunally, you need Root Privilages to shutdown the Operating System \n \n""")
+        printstart(
+            """You can gain access to these by (*insert challange here, example: get the password of the main file*). You will have to overcome multiple challenges \n \n""")
+        printstart("""So, dont waste your time, think smarter not harder, and good luck!
+                    (*Title* starting, 
+                    gaining system access,
+                    Access gained.
+                    AI will launch...) \n \n""")
+        printtree("System", START_PATH)
+        user_input_cmd()
 
 def user_input_cmd():
     print(term.green_on_black(""))
