28b2117b0da3bb5d46bb6b742afea081af0b5543
Objectivitix <itsallobjects@gmail.com>
2021-07-12 16:23:05 -0300

    Did some minor refineries and added a movement mapping

--- gamelogic/controller.py
+++ gamelogic/controller.py
@@ -1,6 +1,6 @@
 from __future__ import division
 
-from typing import Any, List
+from typing import Any, List, Optional, Tuple
 
 from asciimatics.effects import Effect
 from asciimatics.event import Event, KeyboardEvent
@@ -12,16 +12,16 @@ from asciimatics.screen import Screen
 
 class Map(Effect):
     """
-    Draws the map relative to the player position
-    and controls the map in general
+    Draw the map relative to the player position
+    and controls the map in general.
     """
 
-    def __init__(self, screen: Screen, map: List[str]):
+    def __init__(self, screen: Screen, game_map: List[str]):
         super(Map, self).__init__(screen)
         self._screen = screen
-        self._map = map
+        self._map = game_map
 
-        for i, line in enumerate(map):
+        for i, line in enumerate(game_map):
             j = line.find('@')
             if j > -1:
                 self.player_x = j
@@ -37,23 +37,19 @@ class Map(Effect):
     def _update(self, _: Any = None) -> None:
         """
         This function is called every frame, here we draw the player centered at the screen
-        and the maps surrounding it
+        and the maps surrounding it.
         """
-        screen_i = screen_j = 0
         w2 = self._screen.width // 2
         h2 = self._screen.height // 2
-        for offset_i in range(-w2, w2):
-            screen_j = 0
-            for offset_j in range(-h2, h2):
+        for screen_i, offset_i in enumerate(range(-w2, w2)):
+            for screen_j, offset_j in enumerate(range(-h2, h2)):
                 # breakpoint()
                 # print(f"offset: {offset_i},{offset_j}")
                 rel_x, rel_y = self.player_x+offset_i, self.player_y+offset_j
 
                 # print(f"map ({rel_x},{rel_y})")
                 # breakpoint()
-                if rel_x > -1 and rel_x < len(self._map[0]) \
-                        and rel_y > -1 and rel_y < len(self._map):
-
+                if -1 < rel_x < len(self._map[0]) and -1 < rel_y < len(self._map):
                     if offset_i == 0 and offset_j == 0:
                         self._screen._print_at(
                             "@", screen_i, screen_j, 1)
@@ -62,24 +58,22 @@ class Map(Effect):
                             self._map[rel_y][rel_x], screen_i, screen_j, 1)
                 else:
                     self._screen._print_at(" ", screen_i, screen_j, 1)
-                screen_j += 1
-            screen_i += 1
         self._screen._print_at("@", w2, h2, 1)
 
     @property
     def frame_update_count(self) -> int:
-        """Required function for Effects"""
+        """Required function for Effects."""
         # No animation required.
         return 0
 
     @property
     def stop_frame(self) -> int:
-        """Required function for Effects"""
+        """Required function for Effects."""
         # No specific end point for this Effect.  Carry on running forever.
         return 0
 
     def reset(self) -> None:
-        """Required function for Effects"""
+        """Required function for Effects."""
         # Nothing special to do.  Just need this to satisfy the ABC.
         pass
 
@@ -87,7 +81,7 @@ class Map(Effect):
 class GameController(Scene):
     """
     Class responsible for moving the player along the map
-    and controlling the game in general
+    and controlling the game in general.
     """
 
     # Controls collisions
@@ -102,10 +96,10 @@ class GameController(Scene):
         ]
         super(GameController, self).__init__(effects, -1)
 
-    def cast_ray(self, direction: List[int], pos: List[int] = None) -> int:
+    def cast_ray(self, direction: Tuple[int], pos: Optional[List[int]] = None) -> int:
         """
-        Cast a ray into 'direction' starting from 'pos'
-        if 'pos' is not specified we default to player pos
+        Cast a ray into 'direction' starting from 'pos';
+        if 'pos' is not specified we default to player pos.
         """
 
         if pos is None:
@@ -117,37 +111,60 @@ class GameController(Scene):
         if self._map._map[y][x] == " ":
             return GameController.EMPTY_SPACE
 
-    def process_event(self, event: Event) -> Event:
-        """Process events, mostly player input"""
+    def process_event(self, event: Event) -> Optional[Event]:
+        """Process events, mostly player input."""
         # Allow standard event processing first
         if super(GameController, self).process_event(event) is None:
             return
 
+        movement_mappings = [
+            {
+                "trigger_keys": (ord("a"), Screen.KEY_LEFT),
+                "raycast_direction": (-1, 0),
+                "map_movement": ("x", -1),
+            },
+            {
+                "trigger_keys": (ord("d"), Screen.KEY_RIGHT),
+                "raycast_direction": (+1, 0),
+                "map_movement": ("x", +1),
+            },
+            {
+                "trigger_keys": (ord("w"), Screen.KEY_UP),
+                "raycast_direction": (0, -1),
+                "map_movement": ("y", -1),
+            },
+            {
+                "trigger_keys": (ord("s"), Screen.KEY_DOWN),
+                "raycast_direction": (0, +1),
+                "map_movement": ("y", +1),
+            },
+        ]
+
         # If that didn't handle it, check for a key that this demo understands.
         if isinstance(event, KeyboardEvent):
-            c = event.key_code
-            if c in (ord("q"), ord("Q")):
+            key_code = event.key_code
+            not_recognised = True  # Flag for if the key code wasn't recognised
+
+            if key_code in (ord("q"), ord("Q")):
                 raise StopApplication("User exit")
-            elif c in (ord("a"), Screen.KEY_LEFT):
-                # Only move if we are going to an empty space
-                if self.cast_ray([-1, 0]) == GameController.EMPTY_SPACE:
-                    self._map.player_x -= 1
-            elif c in (ord("d"), Screen.KEY_RIGHT):
-                if self.cast_ray([1, 0]) == GameController.EMPTY_SPACE:
-                    self._map.player_x += 1
-            elif c in (ord("w"), Screen.KEY_UP):
-                if self.cast_ray([0, -1]) == GameController.EMPTY_SPACE:
-                    self._map.player_y -= 1
-            elif c in (ord("s"), Screen.KEY_DOWN):
-                if self.cast_ray([0, 1]) == GameController.EMPTY_SPACE:
-                    self._map.player_y += 1
-            # elif c in (ord("m"), ord("M")):
-            #     self._state.show_mini_map = not self._state.show_mini_map
-            #     if self._state.show_mini_map:
-            #         self.add_effect(self._mini_map)
-            else:
+
+            for movement_mapping in movement_mappings:
+                if key_code in movement_mapping["trigger_keys"]:
+                    if (
+                            self.cast_ray(movement_mapping["raycast_direction"])
+                            == GameController.EMPTY_SPACE
+                    ):
+                        axis, move = movement_mapping["map_movement"]
+                        attr = (self._map, f"player_{axis}")
+                        setattr(*attr, getattr(*attr) + move)
+
+                    not_recognised = False
+                    break
+
+            if not_recognised:
                 # Not a recognised key - pass on to other handlers.
                 return event
+
         else:
             # Ignore other types of events.
             return event
