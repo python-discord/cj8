6befcf2d2ef982dd48e941e48253ed3f62e75f84
Tom Junges <bewerbung.cookie.net@gmail.com>
2021-07-12 16:35:55 +0200

    Changes on Portscan to fit with Blessed

--- virtualbox/hints_puzzles/hints.py
+++ virtualbox/hints_puzzles/hints.py
@@ -4,32 +4,32 @@ def portscanner(user_input, fs, user):
             use_true = 'temp'
     except:
         pass
-    "portscan - commits portscan"
     ports = [22, 80, 9929, 8898, 22542, 187, 32312]
     outputs = ['not a hint', 'not a hint', 'not a hint', 'not a hint', 'not a hint', 'a hint', 'a hint', 'a hint', 'a hint']
     if use_true:
-        print(f'Scanning Network for Port: {user_input}')
+        print(term.green_on_black(f'Scanning Network for Port: {user_input}'))
         time.sleep(1)
         clear_term()
-        print(f'f"Found Port in Network: \n    {port}/TCP [State: open] \n    Scanning Port... \n"')
+        print(term.green_on_black(f"Found Port in Network: \n    {port}/TCP [State: open] \n    Scanning Port... \n"))
         time.sleep(1)
         clear_term()
         output = random.choice(outputs)
         clear_term()
-        print(f'Port {inp} attackable. \n    Attack launchend. \n    Output: {output} \n')
+        print(term.green_on_black(f'Port {inp} attackable. \n    Attack launchend. \n    Output: {output} \n'))
     else:
         for i in range(7):
             port = ports[i]
-            print(
-                str(f"Found Port in Network: \n    {port}/TCP [State: open] \n    Scanning Port... \n"))  # term.green_on_black
+            print(term.green_on_black(f"Found Port in Network: \n    {port}/TCP [State: open] \n    Scanning Port... \n"))  # term.green_on_black
             time.sleep(0.4)
-        inp = input('Select a port to scan: ')
+        inp = input(term.green_on_black('Select a port to scan: '))
         inp = int(inp)
-        if inp in ports:
-            output = random.choice(outputs)
-            time.sleep(3)
-            clear_term()
-            print(f'Port {inp} attackable. \n    Attack launchend. \n    Output: {output} \n')
+        with term.cbreak():
+            val = ''
+            if int(val.lower()) in ports:
+                output = random.choice(outputs)
+                time.sleep(3)
+                clear_term()
+                print(term.green_on_black(f'Port {inp} attackable. \n    Attack launchend. \n    Output: {output} \n'))
 
-        else:
-            print('nothing')
+            else:
+                print(term.green_on_black('The Port you entered wasnt found in the Network!'))
