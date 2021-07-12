85bd23a19e2c4abececcd4e8a97e68bd71bf1504
Will <wd14469@my.bristol.ac.uk>
2021-07-12 15:06:10 +0100

    Added DefaultKeyboardHandler using keyboard package.

--- src/frontend/keyboard_handlers.py
+++ src/frontend/keyboard_handlers.py
@@ -1,6 +1,9 @@
 import sys
 from abc import ABC
 
+import keyboard
+from keyboard import KeyboardEvent
+
 from src.backend.core import CoreBackend
 
 
@@ -16,9 +19,24 @@ class BaseKeyboardHandler(ABC):
     def __init__(self, backend: CoreBackend):
         self.backend = backend
 
+    def key_pressed(self, key: str) -> None:
+        """Pass on the key press to the backend module"""
+        # TODO This need to pass on the keypress to the backend
+        ...
+
 
 class DefaultKeyboardHandler(BaseKeyboardHandler):
     """Implements the default handler"""
+
+    def __init__(self, backend: CoreBackend):
+        super().__init__(backend)
+
+        self.keyboard = keyboard.hook(self.key_press_hook)
+
+    def key_press_hook(self, keyboard_event: KeyboardEvent, **kwargs) -> None:
+        """Keyboard callback hook"""
+        # TODO Log key press
+        self.key_pressed(keyboard_event.name)
 
 
 class KeyboardFactory:
@@ -48,3 +66,7 @@ class KeyboardFactory:
 
         # return the default handler
         return DefaultKeyboardHandler(backend)
+
+
+if __name__ == "__main__":
+    test = KeyboardFactory.get(None)
--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
+from src.frontend.core import CoreFrontend
+
 if __name__ == "__main__":
-    ...
+    frontend = CoreFrontend()
+    frontend.backend.new_level()
+    frontend.start_loop()
