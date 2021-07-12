197066ef6b972f989c1613c5402ec97e07a8460a
Fergus <fergusbhaak@gmail.com>
2021-07-12 23:20:18 +0100

    changed how dir works

--- virtualbox/config/__init__.py
+++ virtualbox/config/__init__.py
@@ -1,5 +1,6 @@
 from sys import platform
 import string
+import os
 
 # Create our table of all characters.
 ALL_CHARACTERS = string.ascii_letters+string.digits+string.punctuation+string.whitespace
@@ -14,7 +15,7 @@ etcskel = "/etc/skel"
 template = '{0}─/{1}/{2}{3}'
 
 # Start Path
-START_PATH = "OS"
+START_PATH = os.path.dirname(os.path.abspath(__file__)) + sep + ".." + sep + "OS"
 
 # Blank Lines
 BLANK_LINES = 50
