80cd3ae01912570d8af4b66b18bee909018c80fe
Tom Junges <bewerbung.cookie.net@gmail.com>
2021-07-13 17:16:01 +0200

    Added that Intro is only displayed on first game launch

--- virtualbox/project.py
+++ virtualbox/project.py
@@ -50,33 +50,41 @@ def user_input_cmd(fs, user):
 
 
 def start(fs, user):
+    firstgamefile = open('first_game.txt', 'r')
+    content = firstgamefile.readline()
     clear_term()
-    print_box('Intro',
-       [' Hey There! You are an Artificial Intelligant,',' built by the USA, developed to get into PCs and analyze them.',' You was hacked into a System by the Atomic Program of the Iran.',' Here, your job was to analyze the Data and to see,',' if there are any files which could gives hints to the Atomatic Missiles of the Iran.'])
-    input()
-    clear_term()
-    print_box('Intro',
-       [' You found out that there will be a nuclear',' launch today, it should hit the US. But unfortunally, the system',' is offline, you cant contact the USA to warn them.'])
-    input()
-    clear_term()
-    print_box('Intro',
-       [' Because of that, you have decided that youll try to',' turn of the System, because you found indicates that that will stop',' the attack. But unfortunally, you need Root Privilages to',' shutdown the Operating System '])
-    input()
-    clear_term()
-    print_box('Intro',
-       ['You can gain access to these by (*insert challange here,',' example: get the password of the main file*). ','You will have to overcome multiple challenges'])
-    input()
-    clear_term()
-    print_box('Intro',
-        ['So, dont waste your time, think smarter not harder, and good luck!',
-               '*Title* starting',
-               'gaining system access',
-               'Access gained.',
-               'AI will launch...)'])
-    clear_term()
-    printhelp_first('This is the file tree, here, you can see every file in the operating system!')
-    print_tree("System", fs, user)
-    printhelp_first('First, type "help" in the console to see all of the commands you can use!')
+    if content[0] == '0':
+        print_box('Intro',
+           [' Hey There! You are an Artificial Intelligant,',' built by the USA, developed to get into PCs and analyze them.',' You was hacked into a System by the Atomic Program of the Iran.',' Here, your job was to analyze the Data and to see,',' if there are any files which could gives hints to the Atomatic Missiles of the Iran.'])
+        input()
+        clear_term()
+        print_box('Intro',
+           [' You found out that there will be a nuclear',' launch today, it should hit the US. But unfortunally, the system',' is offline, you cant contact the USA to warn them.'])
+        input()
+        clear_term()
+        print_box('Intro',
+           [' Because of that, you have decided that youll try to',' turn of the System, because you found indicates that that will stop',' the attack. But unfortunally, you need Root Privilages to',' shutdown the Operating System '])
+        input()
+        clear_term()
+        print_box('Intro',
+           ['You can gain access to these by (*insert challange here,',' example: get the password of the main file*). ','You will have to overcome multiple challenges'])
+        input()
+        clear_term()
+        print_box('Intro',
+            ['So, dont waste your time, think smarter not harder, and good luck!',
+                   '*Title* starting',
+                   'gaining system access',
+                   'Access gained.',
+                   'AI will launch...)'])
+        clear_term()
+        printhelp_first('This is the file tree, here, you can see every file in the operating system!')
+        print_tree("System", fs, user)
+        printhelp_first('First, type "help" in the console to see all of the commands you can use!')
+        with open('first_game.txt', 'w') as firstgamefile:
+            firstgamefile.truncate()
+            firstgamefile.write('1')
+    else:
+        print_box('Welcome Back', ['', 'Your Game-State was loaded again! ', ''])
     return
 
 
