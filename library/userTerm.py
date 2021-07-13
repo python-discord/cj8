69b3ab6c832cf740e98b74c2a2387211742ce824
richard <richardphi1618@gmail.com>
2021-07-13 17:27:27 -0400

    added functionality to save previous subgrid

--- library/userTerm.py
+++ library/userTerm.py
@@ -8,9 +8,11 @@ class UserTermState:
     subgrid_select_bool: bool = True
     space_select_bool: bool = True
 
+    wait_for_ready: bool = True
     user_confirm_bool: bool = True
     start_of_turn: bool = True
     end_of_turn: bool = False
+    save_subgrid: bool = True
 
     user_select_subgrid: int = 0
     user_select_space: int = 0
@@ -29,30 +31,30 @@ def starting_user_section() -> list[str]:
     return output
 
 
-def update_user_section(infoToBeAdded: list[str]) -> list[str]:
+def update_user_section(updated_info: list[str]) -> list[str]:
     """To add your information to the terminal you have 3 lines with 31 spaces"""
     output: list[str] = []
 
-    for i in infoToBeAdded:
+    for i in updated_info:
         if len(i) > 31:
             raise NameError("Terminal contents too big")
 
     # theres a better way to do this im just too dumb to think of it atm
-    if len(infoToBeAdded) == 1:
-        infoToBeAdded += " "
-        infoToBeAdded += " "
-        infoToBeAdded += " "
-    elif len(infoToBeAdded) == 2:
-        infoToBeAdded += " "
-        infoToBeAdded += " "
-    elif len(infoToBeAdded) == 3:
-        infoToBeAdded += " "
+    if len(updated_info) == 1:
+        updated_info += " "
+        updated_info += " "
+        updated_info += " "
+    elif len(updated_info) == 2:
+        updated_info += " "
+        updated_info += " "
+    elif len(updated_info) == 3:
+        updated_info += " "
 
     output += "┌─term────────────────────────────┐"
-    output += "\n│ " + "".join(infoToBeAdded[0]).ljust(31) + " │"
-    output += "\n│ " + "".join(infoToBeAdded[1]).ljust(31) + " │"
-    output += "\n│ " + "".join(infoToBeAdded[2]).ljust(31) + " │"
-    output += "\n│ " + "".join(infoToBeAdded[3]).ljust(31) + " │"
+    output += "\n│ " + "".join(updated_info[0]).ljust(31) + " │"
+    output += "\n│ " + "".join(updated_info[1]).ljust(31) + " │"
+    output += "\n│ " + "".join(updated_info[2]).ljust(31) + " │"
+    output += "\n│ " + "".join(updated_info[3]).ljust(31) + " │"
     output += "\n└(Enter to confirm)───('q' to esc)┘"
 
     return output
