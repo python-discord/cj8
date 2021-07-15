8e044608bb1df2169f8e8b63ca60b06efca3d34d
richard <richardphi1618@gmail.com>
2021-07-14 22:15:04 -0400

    first stab at structure. Buggy but basic idea present

--- library/user_term.py
+++ library/user_term.py
@@ -33,7 +33,24 @@ def starting_user_section() -> str:
     return output
 
 
-def update_user_section(updated_info: list[str]) -> list[str]:
+def convert_to_space_location(space_num: int) -> list:
+    """Convert int to grid location"""
+    grid_map = {
+        1: [2, 0],
+        2: [2, 1],
+        3: [2, 2],
+        4: [1, 0],
+        5: [1, 1],
+        6: [1, 2],
+        7: [0, 0],
+        8: [0, 1],
+        9: [0, 2],
+    }
+
+    return grid_map[space_num]
+
+
+def update_user_section(updated_info: list[str]) -> str:
     """To add your information to the terminal you have 3 lines with 31 spaces"""
     item: list[str] = []
 
@@ -62,3 +79,7 @@ def update_user_section(updated_info: list[str]) -> list[str]:
     output = "".join(update_user_section(item))
 
     return output
+
+
+if __name__ == "__main__":
+    print(convert_to_space_location(1))
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from blessed import Terminal
 # custom libraries
 from library.board import Board
 from library.game_state import GameState
+from library.user_term import convert_to_space_location
 
 term = Terminal()
 board = Board()
@@ -23,8 +24,21 @@ with term.fullscreen(), term.cbreak(), term.hidden_cursor():
     while (val := term.inkey()) != "q":
 
         if val.is_sequence:
-            state.user_input = val.name
+            state.user_input = str(val.name)
         else:
-            state.user_input = val
+            state.user_input = str(val)
 
         state.driver(term)
+
+        if state.update_board:
+            working_space_location = convert_to_space_location(state.user_select_space)
+            if state.player_active == 1:
+                board.collect_subgrid(str(state.user_select_subgrid - 1))[
+                    working_space_location[0], working_space_location[1]
+                ] = "X"
+            elif state.player_active == 2:
+                board.collect_subgrid(str(state.user_select_subgrid - 1))[
+                    working_space_location[0], working_space_location[1]
+                ] = "0"
+
+            board.draw_board(term)
