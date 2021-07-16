e6c68cd2069afe1022dd51d0072365cacba6f502
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-15 18:29:47 -1000

    Enemy movement bug fix (#50)
    * Enemies cannot walk into doors
    
    * Preventing enemies from teleporting across screen

--- src/resources/GameResources.py
+++ src/resources/GameResources.py
@@ -28,10 +28,16 @@ class GameResources:
         x = entity.new_positions["x"]
         y = entity.new_positions["y"]
         try:
-            if str(self.level.board[entity.y + y][entity.x + x]) in ("'", "$", "@", "#"):
+            if str(self.level.board[entity.y + y][entity.x + x]) in ("'", "$", "@") or \
+                    (entity.__class__.__name__ != 'Enemy' and str(
+                        self.level.board[entity.y + y][entity.x + x]) == '#'):
                 self.level.board[entity.y][entity.x] = entity.ground_symbol
-                entity.x += x
-                entity.y += y
+                if entity.x + x > 0 and entity.y + y > 0:
+                    entity.x += x
+                    entity.y += y
+                else:
+                    entity.x -= x
+                    entity.y -= y
                 entity.ground_symbol = self.level.board[entity.y][entity.x]
                 entity.new_positions = {"x": 0, "y": 0}
         except IndexError:
