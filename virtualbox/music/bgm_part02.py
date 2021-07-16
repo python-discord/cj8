5f6dbe5f8c73ad0092d396d5d2621415b45f1fbb
Shirley <shirleyyuezou@gmail.com>
2021-07-16 21:59:12 +0100

    update bgm

--- virtualbox/music/bgm_part02.py
+++ virtualbox/music/bgm_part02.py
@@ -17,6 +17,12 @@ fitMedia(RD_EDM_MAINBEAT_18, 9, 7, 8)
 fitMedia(RD_EDM_MAINBEAT_15, 10, 1, 9)
 fitMedia(YG_HIP_HOP_PIANO_2, 11, 1, 2)
 fitMedia(YG_HIP_HOP_PIANO_2, 11, 5, 7)
-setEffect(11, VOLUME, GAIN, 5, 1, 5, 9)
+fitMedia(HOUSE_DEEP_DREAMPAD_002, 12, 1, 2.5)
+fitMedia(HOUSE_SFX_WHOOSH_001, 13, 8, 9)
+
+setEffect(12,VOLUME,GAIN,10,1,10,9)
+setEffect(13,VOLUME,GAIN,0,1,0,9)
+setEffect(11,VOLUME,GAIN,5,1,5,9)
+setEffect(MIX_TRACK,VOLUME,GAIN,-45,1,-45,9)
 
 finish()
