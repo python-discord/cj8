ab4d6c03dd225b0cd85419ea991c594c4567f1de
Cheepsss <mihaelagrajdan3102003@gmail.com>
2021-07-17 16:59:56 +0300

    added level 2 and level3

--- maps.py
+++ maps.py
@@ -25,7 +25,7 @@ class Map:
         self.boxes_rect: List[physics2.Object] = []
         self.targets = []
         self.platforms = []
-        self.space = physics2.Space(100, 30, gravity=20, upscale=100)
+        self.space = physics2.Space(100, 30, gravity=10, upscale=100)
 
     def create_level1(self):
         self.player = Player(10, 26, self.terminal)
@@ -63,16 +63,117 @@ class Map:
             box.x, box.y = self.boxes_rect[i].get_position()
 
     def create_level2(self):
-        self.player = Player(10, 26, self.terminal)
+        self.player = Player(35, 4, self.terminal)
+        player_rect = self.space.add_object(35, 4, PLAYER_W, PLAYER_H, type="player")
+        self.player_rect = player_rect
 
-        self.thinking_box = ThinkingBox(15, 26, self.terminal)
+        self.thinking_box = ThinkingBox(20, 4, self.terminal)
 
-        border1 = Platform(0, 30, 100, True, self.terminal)
-        platform1 = Platform(0, 7, 20, True, self.terminal)
-        platform2 = Platform(0, 25, 20, True, self.terminal)
-        self.platforms.append(platform1)
-        self.platforms.append(platform2)
+        box1 = Box(6, 5, self.terminal)
+        box1_rect = self.space.add_object(6, 5, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box1_rect)
+        self.boxes.append(box1)
+
+        box2 = Box(47, 12, self.terminal)
+        box2_rect = self.space.add_object(47, 12, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box2_rect)
+        self.boxes.append(box2)
+        box3 = Box(55, 12, self.terminal)
+        box3_rect = self.space.add_object(55, 12, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box3_rect)
+        self.boxes.append(box3)
+
+
+        border1 = Platform(0, 29, 100, True, self.terminal)
+        self.space.add_object(0, 29, 100, PLATFORM_H, type="platform")
         self.platforms.append(border1)
+        
+        platform1 = Platform(0, 7, 40, True, self.terminal)
+        self.space.add_object(0, 7, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform1)
+
+        platform2 = Platform(0, 26, 40, True, self.terminal)
+        self.space.add_object(0, 26, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform2)
+
+        platform3 = Platform(44, 14, 30, True, self.terminal)
+        self.space.add_object(44, 14, 30, PLATFORM_H, type="platform")
+        self.platforms.append(platform3)
+
+        target1 = Target(36, 27, self.terminal)
+        self.space.add_object(36, 27, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target1)
+        target1 = Target(90, 27, self.terminal)
+        self.space.add_object(90, 27, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target1)
+        target1 = Target(0, 24, self.terminal)
+        self.space.add_object(70, 21, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target1)
+
+    def create_level3(self):
+        self.player = Player(25, 4, self.terminal)
+        player_rect = self.space.add_object(25, 4, PLAYER_W, PLAYER_H, type="player")
+        self.player_rect = player_rect
+
+        self.thinking_box = ThinkingBox(15, 4, self.terminal)
+        
+        border1 = Platform(0, 29, 100, True, self.terminal)
+        self.space.add_object(0, 29, 100, PLATFORM_H, type="platform")
+        self.platforms.append(border1)
+
+        platform1 = Platform(0, 7, 40, True, self.terminal)
+        self.space.add_object(0, 7, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform1)
+
+        platform2 = Platform(44, 10, 40, True, self.terminal)
+        self.space.add_object(44, 10, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform2)
+
+        platform3 = Platform(0, 16, 40, True, self.terminal)
+        self.space.add_object(0, 16, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform3)
+
+        platform4 = Platform(0, 24, 40, True, self.terminal)
+        self.space.add_object(0, 24, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform4)
+
+        platform5 = Platform(44, 24, 40, True, self.terminal)
+        self.space.add_object(44, 24, 40, PLATFORM_H, type="platform")
+        self.platforms.append(platform5)
+
+        box1 = Box(35, 5, self.terminal)
+        box1_rect = self.space.add_object(35, 5, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box1_rect)
+        self.boxes.append(box1)
+
+        box2 = Box(50, 8, self.terminal)
+        box2_rect = self.space.add_object(50, 8, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box2_rect)
+        self.boxes.append(box2)
+        box3 = Box(60, 8, self.terminal)
+        box3_rect = self.space.add_object(60, 8, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box3_rect)
+        self.boxes.append(box3)
+
+        box4 = Box(20, 14, self.terminal)
+        box4_rect = self.space.add_object(20, 14, BOX_W, BOX_H, type="box")
+        self.boxes_rect.append(box4_rect)
+        self.boxes.append(box4)
+
+        target1 = Target(0, 27, self.terminal)
+        self.space.add_object(0, 27, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target1)
+        target2 = Target(0, 25, self.terminal)
+        self.space.add_object(0, 25, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target2)
+        target3 = Target(0, 22, self.terminal)
+        self.space.add_object(0, 22, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target3)
+        target4 = Target(90, 27, self.terminal)
+        self.space.add_object(90, 27, TARGET_W, TARGET_H, type="target")
+        self.targets.append(target4)
+
+
 
     def delete(self):
         print(self.terminal.home + self.terminal.on_midnightblue + self.terminal.clear(), flush=True)
