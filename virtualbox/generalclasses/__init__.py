97bd8d174cad8fb8c072c19739a55f7c582afa20
Fergus <fergusbhaak@gmail.com>
2021-07-17 17:23:53 +0100

    reset main

--- virtualbox/generalclasses/__init__.py
+++
@@ -1,7 +0,0 @@
-class copy:
-    """Just soteis copy method"""
-
-    def copy(self, other: 'copy') -> None:
-        """Copies other to self(other is 2nd name obvously)"""
-        for i in self.__init__.__code__.co_varnames[1:self.__init__.__code__.co_argcount]:
-            setattr(self, i, getattr(other, i))
